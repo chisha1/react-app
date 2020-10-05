d418e2c8c6fb8620b59580aa212be225dbcae5c8
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-10-05 11:35:48 +0100

    Created React App

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components'
+import Header from '../Header/Header';
+import Menu from '../Menu/Menu';
+import Footer from '../Footer/Footer';
+
+const LayoutContainer = styled.div`
+  margin: 3px, 4px;
+`
+
+const Layout = ({ children }) => (
+    <LayoutContainer>
+        <Header />
+        <Menu />
+        {children}
+        <Footer />
+    </LayoutContainer>
+);
+
+export default Layout
