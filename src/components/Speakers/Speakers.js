c48117502f01f57220de3ec2db7495adb25322b9
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-10-07 12:20:33 +0100

    Transition from Array to REST service for Speaker Data & Implemented Data Status for Loading and Errors

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,46 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Speaker from '../Speaker/Speaker';
+import axios from 'axios';
 import SpeakersSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 
 const Speakers = () => {
-    const speakersArray = [
-    {
-      imageSrc: 'speaker-component-1124',
-      name: 'Douglas Crockford',
-      id: 1124,
-      firstName: 'Douglas',
-      lastName: 'Crockford',
-      sat: true,
-      sun: false,
-      isFavourite: false,
-      bio:
-        'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
-    },
-    {
-      imageSrc: 'speaker-component-1530',
-      name: 'Tamara Baker',
-      id: 1530,
-      firstName: 'Tamara',
-      lastName: 'Baker',
-      sat: false,
-      sun: true,
-        isFavourite: true,
-      bio:
-        'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
-    },
-    {
-      imageSrc: 'speaker-component-10803',
-      name: 'Eugene Chuvyrov',
-      id: 10803,
-      firstName: 'Eugene',
-      lastName: 'Chuvyrov',
-      sat: true,
-      sun: false,
-      isFavourite: false,
-      bio:
-        'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
-    },
-    ];
 
     function toggleSpeakerFavourite(speakerRec) {
         return {
@@ -49,15 +12,49 @@ const Speakers = () => {
         };
     }
 
-    function favouriteToggleHandler(speakerRec) {
+    async function favouriteToggleHandler(speakerRec) {
         const toggledSpeakerRec = toggleSpeakerFavourite(speakerRec); //pass in original records
         const speakerIndex = speakers.map((speaker) => speaker.id).indexOf(speakerRec.id); //get the index of the speaker to change
-        setSpeakers //create a new array of speakers, setting the state
-            ([...speakers.slice(0, speakerIndex), toggledSpeakerRec, ...speakers.slice(speakerIndex + 1)]);
+
+        try {
+            await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
+            setSpeakers //create a new array of speakers, setting the state
+                ([...speakers.slice(0, speakerIndex), toggledSpeakerRec, ...speakers.slice(speakerIndex + 1)]);
+        } catch (e) {
+            setStatus(REQUEST_STATUS.ERROR);
+            setError(e);
+        }
     }
 
     const [searchQuery, setSearchQuery] = useState("");
-    const [speakers, setSpeakers] = useState(speakersArray);
+    const [speakers, setSpeakers] = useState([]);
+
+    const REQUEST_STATUS = {
+        LOADING: "loading",
+        SUCCESS: "success",
+        ERROR: "error"
+    }
+
+    const [status, setStatus] = useState(REQUEST_STATUS.LOADING);
+    const [error, setError] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:4000/speakers");
+                setSpeakers(response.data);
+                setStatus(REQUEST_STATUS.SUCCESS);
+            } catch (e) {
+                setStatus(REQUEST_STATUS.ERROR);
+                setError(e);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const success = status === REQUEST_STATUS.SUCCESS;
+    const isLoading = status === REQUEST_STATUS.LOADING;
+    const hasErrored = status === REQUEST_STATUS.ERROR;
 
     return (
         <div>
@@ -65,20 +62,30 @@ const Speakers = () => {
                 searchQuery={searchQuery}
                 setSearchQuery={setSearchQuery}
             />
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
-                {speakers
-                    .filter((rec) => {
-                        const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
-                        return searchQuery.length === 0
-                            ? true
-                            : targetString.includes(searchQuery.toLowerCase());
-                    })
-                    .map((speaker) => (
-                        <Speaker key={speaker.id} {...speaker}
-                            favouriteToggle={() => favouriteToggleHandler(speaker)}
-                        />
-        ))}
-        </div>
+            {isLoading && <div>Loading...</div>}
+            {hasErrored && (
+                <div>
+                    Loading error...
+                    <br />
+                    <b>ERROR: {error.message}</b>
+                </div>
+            )}
+            {success && (
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
+                    {speakers
+                        .filter((rec) => {
+                            const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
+                            return searchQuery.length === 0
+                                ? true
+                                : targetString.includes(searchQuery.toLowerCase());
+                        })
+                        .map((speaker) => (
+                            <Speaker key={speaker.id} {...speaker}
+                                favouriteToggle={() => favouriteToggleHandler(speaker)}
+                            />
+                        ))}
+                </div>
+            )}
     </div>
     );
 };
