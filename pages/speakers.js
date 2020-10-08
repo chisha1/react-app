071e08fe883fa530932e1f7188d5c8648b18accf
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-10-08 15:53:37 +0100

    Minor styling changes

--- pages/speakers.js
+++ pages/speakers.js
@@ -4,7 +4,7 @@ import Layout from '../src/components/Layout/Layout';
 export default function Page() {
   return (
     <Layout>
-      <Speakers bgColour="bg-gray-500" />
+      <Speakers bgColour="bg-gray-200" />
     </Layout>
   );
 }
