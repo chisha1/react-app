86df1a01bf6f8febc3542a265c4953f862022f0b
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-10-08 14:34:14 +0100

    Implemeneted HOC (Higher-order Component) for Speakers Component

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,64 +1,21 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState } from 'react';
 import Speaker from '../Speaker/Speaker';
-import axios from 'axios';
 import SpeakersSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
-import requestReducer, { REQUEST_STATUS } from '../../reducers/request';
+import { REQUEST_STATUS } from '../../reducers/request';
+import withRequest from '../../HOCs/withRequest';
 
-import {
-    GET_ALL_SUCCESS,
-    GET_ALL_FAILURE,
-    PUT_SUCCESS,
-    PUT_FAILURE
-} from '../../actions/request';
 
-const Speakers = () => {
+const Speakers = ({ records: speakers, status, error, put }) => {
 
     const favouriteToggleHandler = async (speakerRec) => {
-        try {
-            const toggledSpeakerRec = {
-                ...speakerRec,
-                isFavourite: !speakerRec.isFavourite
-            };
-            await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
-            dispatch({
-                type: PUT_SUCCESS,
-                record: toggledSpeakerRec,
-            });
-        } catch (e) {
-            dispatch({
-                type: PUT_FAILURE,
-                error: e,
-            })
-        }
+        put({
+            ...speakerRec,
+            isFavourite: !speakerRec.isFavourite
+        })
 
     }
 
     const [searchQuery, setSearchQuery] = useState("");
-
-    const [{ records: speakers, status, error }, dispatch] = useReducer(requestReducer, {
-        records: [],
-        status: REQUEST_STATUS.LOADING,
-        error: null,
-    });
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("http://localhost:4000/speakers");
-                dispatch({
-                    type: GET_ALL_SUCCESS,
-                    records: response.data
-                });
-            } catch (e) {
-                console.log('Loading data error', e)
-                dispatch({
-                    type: GET_ALL_FAILURE,
-                    error: e,
-                });
-            }
-        }
-        fetchData();
-    }, []);
 
     const success = status === REQUEST_STATUS.SUCCESS;
     const isLoading = status === REQUEST_STATUS.LOADING;
@@ -97,4 +54,4 @@ const Speakers = () => {
     </div>
     );
 };
-export default Speakers;
+export default withRequest('http://localhost:4000', 'speakers')(Speakers);
