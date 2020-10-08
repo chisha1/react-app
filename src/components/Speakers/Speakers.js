a356807c79074f5819219d85f2ee5d2c4d753eab
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-10-08 15:54:51 +0100

    Combining multiple HOCs on a Single Component (Speakers)

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -3,9 +3,12 @@ import Speaker from '../Speaker/Speaker';
 import SpeakersSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 import { REQUEST_STATUS } from '../../reducers/request';
 import withRequest from '../../HOCs/withRequest';
+import withSpecialMessage from '../../HOCs/withSpecialMessage';
+import { compose } from 'recompose';
 
 
-const Speakers = ({ records: speakers, status, error, put, bgColour }) => {
+const Speakers = ({ records: speakers, status, error, put, bgColour,
+    specialMessage }) => {
 
     const favouriteToggleHandler = async (speakerRec) => {
         put({
@@ -21,12 +24,23 @@ const Speakers = ({ records: speakers, status, error, put, bgColour }) => {
     const isLoading = status === REQUEST_STATUS.LOADING;
     const hasErrored = status === REQUEST_STATUS.ERROR;
 
-    return (
+    return ( //TODO: move special message to its own component
         <div className={ bgColour }>
             <SpeakersSearchBar
                 searchQuery={searchQuery}
                 setSearchQuery={setSearchQuery}
             />
+
+            {specialMessage && specialMessage.length > 0 && (
+                <div
+                    className="bg-orange-100 border-l-8 border-orange-500 text-orange-700 p-4 text-2xl"
+                    role="alert"
+                >
+                    <p className="font-bold">Special Message</p>
+                    <p>{specialMessage}</p>
+                </div>
+            )}
+
             {isLoading && <div>Loading...</div>}
             {hasErrored && (
                 <div>
@@ -54,4 +68,10 @@ const Speakers = ({ records: speakers, status, error, put, bgColour }) => {
     </div>
     );
 };
-export default withRequest('http://localhost:4000', 'speakers')(Speakers);
+//export default withSpecialMessage(
+//    withRequest('http://localhost:4000', 'speakers')(Speakers));
+
+export default compose(
+    withRequest('http://localhost:4000', 'speakers'),
+    withSpecialMessage()
+)(Speakers);
